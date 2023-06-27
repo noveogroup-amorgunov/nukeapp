376ba895b623e04c77cde55c37557e74456f4843
Yuri <118747540+sldk-yuri@users.noreply.github.com>
2023-06-27 16:03:50 +0300

    feat(app): Add private routes (#16)

--- src/app/appRouter.tsx
+++ src/app/appRouter.tsx
@@ -1,14 +1,41 @@
-import { createBrowserRouter } from 'react-router-dom'
+import { type ReactNode } from 'react'
+import { Navigate, createBrowserRouter } from 'react-router-dom'
 import { featureToggleLoader } from '@/entities/featureToggle'
+import { selectIsAuthorized } from '@/entities/session'
 import { CartPage } from '@/pages/cart'
 import { CategoryPage } from '@/pages/category'
 import { LoginPage } from '@/pages/login'
 import { MainPage } from '@/pages/main'
 import { ProductPage } from '@/pages/product'
 import { WishlistPage } from '@/pages/wishlist'
+import { useAppSelector } from '@/shared/model'
 import { appStore } from './appStore'
 import { baseLayout } from './layouts/baseLayout'
 import { layoutWithSidebar } from './layouts/layoutWithSidebar'
+
+type GuestGuardProps = {
+  children: ReactNode
+}
+
+function GuestGuard({ children }: GuestGuardProps) {
+  const isAuthorized = useAppSelector(selectIsAuthorized)
+
+  if (!isAuthorized) return <Navigate to="/login" />
+
+  return children
+}
+
+type AuthGuardProps = {
+  children: ReactNode
+}
+
+function AuthGuard({ children }: AuthGuardProps) {
+  const isAuthorized = useAppSelector(selectIsAuthorized)
+
+  if (isAuthorized) return <Navigate to="/" />
+
+  return children
+}
 
 export const appRouter = () =>
   createBrowserRouter([
@@ -21,15 +48,27 @@ export const appRouter = () =>
       children: [
         {
           path: '/login',
-          element: <LoginPage />,
+          element: (
+            <AuthGuard>
+              <LoginPage />
+            </AuthGuard>
+          ),
         },
         {
           path: '/user/wishlist',
-          element: <WishlistPage />,
+          element: (
+            <GuestGuard>
+              <WishlistPage />
+            </GuestGuard>
+          ),
         },
         {
           path: '/user/cart',
-          element: <CartPage />,
+          element: (
+            <GuestGuard>
+              <CartPage />
+            </GuestGuard>
+          ),
         },
         {
           path: '/category/:categoryId',
