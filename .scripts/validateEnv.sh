e3bfb2726cd16ed654286f2f44d3e1efa19c5104
amorgunov <emmalahana@yandex.ru>
2026-09-02 14:38:23 +0300

    fix: validateEnv script for typescript 7 (--ignoreConfig, bundler resolution)

--- .scripts/validateEnv.sh
+++ .scripts/validateEnv.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-npx tsc src/shared/lib/env.ts --skipLibCheck --module esnext --moduleResolution node --types 'vite/client'
+npx tsc src/shared/lib/env.ts --ignoreConfig --skipLibCheck --module esnext --moduleResolution bundler --types 'vite/client'
 mv src/shared/lib/env.js src/shared/lib/env--compiled.mjs
 sed -i '' -e 's/import.meta.env/process.env/g' src/shared/lib/env--compiled.mjs
 node -r dotenv/config src/shared/lib/env--compiled.mjs dotenv_config_path=.env dotenv_config_path=.env.local
