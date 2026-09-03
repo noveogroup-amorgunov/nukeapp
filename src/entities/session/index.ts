34368dc684a5620ee8325e2abde688b6043973d9
Alexander <emmalahana@yandex.ru>
2026-09-03 11:53:44 +0300

    refactor: attach api access token without linking session store to shared layer (#44)

--- src/entities/session/index.ts
+++ src/entities/session/index.ts
@@ -1,11 +1,13 @@
 import { sessionSlice } from './model/slice'
 
 export { sessionApi } from './api/sessionApi'
+export { accessTokenSyncMiddleware } from './model/accessTokenSyncMiddleware'
 export { sessionSlice } from './model/slice'
 
 export const {
   isAuthorized: selectIsAuthorized,
   userId: selectUserId,
+  accessToken: selectAccessToken,
 } = sessionSlice.selectors
 
 export const resetSessionData = sessionSlice.actions.reset
